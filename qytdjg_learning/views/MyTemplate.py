72a6d6153f6ca6bf9fccad76612450fdaf83d9fd
collinsctk <collinsctk@qytang.com>
2018-07-31 14:51:14 +0800

    模板学习完毕!

--- qytdjg_learning/views/MyTemplate.py
+++ qytdjg_learning/views/MyTemplate.py
@@ -124,7 +124,7 @@ def loadTemplateFile1(request):
     return render(request, 'temp1.html', {'current_date':now})
 
 def loadTemplateFile2(request):
-    now = datetime.datetime.now()
+    current_date = datetime.datetime.now()
     field1 = 'hello'
     field2 = 'world'
     field3 = 'qytang'
@@ -134,6 +134,107 @@ def loadTemplateFile2(request):
 # 修改模板目录
 # settings.py
 # 'DIRS': [os.path.join(BASE_DIR, 'templates')] ,继续添加目录
+
+def ifelseTempalte(request):
+    current_date = datetime.datetime.now()
+    field1 = 'hello'
+    field2 = 'world'
+    field3 = 'qytang'
+    today_is_weekend = False
+    return render(request, 'temp3_if.html', locals())
+
+"""
+下面的情况,相当于布尔值False
+1.空列表 []
+2.空元祖 ()
+3.空字典 {}
+4.空字符串 ''
+5.零值 0
+6.None
+7.False
+"""
+
+# if-else标签多值条件与嵌套
+# and or not
+def multicondition(request):
+    current_date = datetime.datetime.now()
+    field1 = 'hello'
+    field2 = 'world'
+    field3 = 'qytang'
+    today_is_weekend = False
+    return render(request, 'temp3_if.html', locals())
+
+# for标签
+"""
+for X in Y:
+
+{% for %}
+
+{% endfor%}
+
+for 标签不支持break和continue
+
+内置变量forloop.counter (从1开始) , forloop.counter0(从0开始)
+forloop.revcounter 和 forloop.revcounter0
+表示循环中剩余项的值
+
+forloop.first(第一次执行for,为Trure) 和 forloop.last(最后一次执行for,为False)
+
+"""
+def forList(request):
+    persons = [{'name':'tina', 'age':32}, {'name':'collinsctk', 'age':39}, {'name':'ender', 'age':36}]
+    return render(request, 'temp4_for.html', locals())
+
+# ifequal 和 ifnotqual
+# 这两个标签只能使用字符串,整数和浮点,不能使用字典,列表,布尔
+def equalVar(request):
+    user = 'collinsctk'
+    currentuser = 'collinsctk1'
+    return render(request, 'temp5.equal.html', locals())
+#     {# 单行注释 #}
+#     {% comment %}
+#     多行注释
+#     <!--abcd--> html注释会出现在页面里边
+#     多行注释
+#     {% endcomment %}
+
+"""
+模板过滤器:对模板变量的二次加工
+1.low:将模板变量的值都变成小写
+2.upper:将模板变量的值都变成大写
+3.trucatewords:截取前n个单词
+4.addslashes:在任何的反斜杠,单引号或双引号前面添加反斜杠
+5.date:按指定的格式化字符串参数格式化date或datetime对象
+6.length:用于返回模板变量的长度
+"""
+def temp_filter(request):
+    mytime = datetime.datetime.now()
+    name = 'collinsctk'
+    productName = 'iPhone, qytang, cisco, sina'
+    return render(request, "temp6_filter.html", locals())
+
+
+# 引用模板(include模板)
+def include_temp(request):
+    main1 = 'Hello world'
+    main2 = 'I love you'
+    main3 = 'It is hot'
+    t1 = '这是第一个模板'
+    t2 = 'This is a sub template'
+    t3 = '这是最后一个模板'
+    return render(request, "temp_main.html", locals())
+
+# 模板继承
+# block标签的名字在同一个模板中不能有重复
+def sub1(request):
+    mobile_product1='iPhone'
+    mobile_product2='Huawei'
+    return render(request, "temp_inherit_sub1.html", locals())
+
+def sub2(request):
+    car_product1 = '特斯拉'
+    car_product2 = '宝马'
+    return render(request, "temp_inherit_sub2.html", locals())
 
 if __name__ == "__main__":
     pass
